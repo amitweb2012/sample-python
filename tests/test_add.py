42048147712efd0e45e6c65d12f8b95e1ac6fab8
Amit <amit.web2012@gmail.com>
2026-02-08 04:44:46 +0530

    most case

--- tests/test_add.py
+++ tests/test_add.py
@@ -40,3 +40,46 @@ def test_is_even_true():
 
 def test_is_even_false():
     assert is_even(7) is False
+
+def test_is_even_zero():
+    assert is_even(0) is True
+
+def test_is_even_negative():
+    assert is_even(-2) is False
+
+def test_is_even_float():
+    assert is_even(3.14) is False
+
+def test_is_even_string():
+    assert is_even("hello") is False
+
+def test_is_even_list():
+    assert is_even([1, 2, 3]) is False
+
+def test_is_even_none():
+    assert is_even(None) is False
+
+def test_is_even_dict():
+    assert is_even({"a": 1, "b": 2}) is False
+
+def test_is_even_tuple():
+    assert is_even((1, 2, 3)) is False
+
+def test_is_even_set():
+    assert is_even({1, 2, 3}) is False
+
+def test_is_even_large_number():
+    assert is_even(1000000) is True
+
+def test_is_even_negative_large_number():
+    assert is_even(-1000000) is True
+
+def test_is_even_non_integer():
+    assert is_even(2.5) is False
+
+def test_is_even_boolean():
+    assert is_even(True) is False
+    assert is_even(False) is True
+
+def test_is_even_empty_string():
+    assert is_even("") is False
