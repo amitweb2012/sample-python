28d31fedb645b61a6fb95f21accd84824968be78
Amit <amit.web2012@gmail.com>
2026-02-16 03:26:11 +0530

    add swagger

--- app/swagger.py
+++ app/swagger.py
@@ -2,7 +2,16 @@ from flasgger import Swagger
 
 def init_swagger(app):
     app.config["SWAGGER"] = {
-        "title": "Flask Calculator API",
-        "uiversion": 3
+      "headers": [],
+      "specs": [
+          {
+              "endpoint": 'apispec',
+              "route": '/docs.json',
+              "rule_filter": lambda rule: True,
+              "model_filter": lambda tag: True,
+          }
+      ],
+      "swagger_ui": True,
+      "specs_route": "/docs/"
     }
     Swagger(app)
